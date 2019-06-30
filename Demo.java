4ab2aea9f5c7e1cdfca040e6d94532e9e79fc505
xinxiaoboDing <52122370+xinxiaoboDing@users.noreply.github.com>
2019-06-30 15:06:16 +0800

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package com.example.demo.test;
+
+
+public class Demo {
+    public static int jiafa()
+    {
+        int i=1;
+        int j=2;
+        int c=i+j;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(jiafa());
+    }
+}
